666fd8f005f773a5a332e5f24cb71fa2063ded6d
Sören <37670358+Romikan@users.noreply.github.com>
2023-06-20 16:14:10 +0200

    not ready

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,34 @@ public class Main {
     public static void main(String[] args) {
 
         initTower(4,1,2,3);
-        shuffleTower(4, 1,2,3);
+        int counter = shuffleTower(4, 1,2,3);
+        System.out.println("Moves: " + counter);
     }
+
     static int counter = 0;
+    private static int getMoves() {
+        return 0;
+    }
     private static void showTower() {
-        System.out.println( "shuffleTower( "+ counter + ", " + N + ", " + src + ", " + tmp + ", " + dst + ")");
     }
     private static void initTower(int N, int src, int tmp, int dst) {
 
+        Turm turm;
+        //turm = new Turm();
+        //turm.N = N;
+        //turm.src = src;
+        //turm.dst = dst;
+        final Turm source = new Turm("A");
+        final Turm template = new Turm("B");
+        final Turm destination = new Turm("C");
+
+
+
+
         System.out.println( "shuffleTower(Zähler, Höhe, Quelle, Ablage, Ziel )");
         System.out.println( "------------------------------------------");
-
     }
-    private static void shuffleTower(int N, int src, int tmp, int dst) {
+    private static int shuffleTower(int N, int src, int tmp, int dst) {
         counter++;
         System.out.println( "shuffleTower( "+ counter + ", " + N + ", " + src + ", " + tmp + ", " + dst + ")");
         if (N == 1) {
@@ -24,8 +39,8 @@ public class Main {
 
             shuffleTower(N - 1, src, dst, tmp);
             System.out.println("Move from " + src + " to " + dst);
-
             shuffleTower(N - 1, tmp, src, dst);
         }
+        return counter;
     }
 }
---
+++ src/Turm.java
@@ -0,0 +1,10 @@
+public class Turm {
+    public static int N;
+    public static int src;
+    public static int tmp;
+    public static int dst;
+
+
+    public Turm(String a) {
+    }
+}
